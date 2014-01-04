b539c9583b84407db052b00073644dd64317d0fc
qubir <support@anichips.com>
2014-01-04 13:48:08 +0800

    build phoenix-3.4

--- daily_build/android_clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR=/home/build/dailybuild
-DATE0=$(date +%w)
-rm -fr "$DIR"/*"$DATE0"
--- daily_build/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DATE0=$(date +%w)
-DIR=/home/panlong/daily_build
-rm -rf "$DIR"/"$DATE0"*
